422f918ab6a8b3092cf3fee85bc990bac478ac26
Miningflo <florian.debosscher@gmail.com>
2018-11-05 23:33:14 +0100

    Add files via upload

---
+++ Sideprojects/Music Project/electron/init.bat
@@ -0,0 +1,6 @@
+npm init
+npm install --save-dev electron
+npm i node-static
+npm install electron-packager --save-dev
+npm install --save-dev electron
+::Now when that is done open up package.json and add a productname
