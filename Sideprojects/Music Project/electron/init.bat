719c40640ce403d1c8d93cd2bf4914903994ca5a
Miningflo <florian.debosscher@gmail.com>
2018-11-11 11:02:30 +0100

    Delete init.bat

--- Sideprojects/Music Project/electron/init.bat
+++
@@ -1,6 +0,0 @@
-npm init
-npm install --save-dev electron
-npm i node-static
-npm install electron-packager --save-dev
-npm install --save-dev electron
-::Now when that is done open up package.json and add a productname
